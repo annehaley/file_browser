9c2b580522434673c86254d22a09423a9d906313
Anne Haley <anne.haley@kitware.com>
2023-01-11 13:59:16 -0500

    Begin building component

--- src/components/FileBrowser.vue
+++ src/components/FileBrowser.vue
@@ -1,9 +1,30 @@
 <script lang="ts">
 export default {
-  props: {},
+  props: {
+    localDirectories: {
+      type: Array,
+      required: true,
+    },
+    remoteDirectories: {
+      type: Array,
+      required: true,
+    },
+    currentLocalDir: {
+      type: String,
+      default: undefined,
+    },
+    currentRemoteDir: {
+      type: String,
+      default: undefined,
+    },
+  },
   data() {
     return {
-      xPosition: 0,
+      dialogOpen: false,
+      syncCurrentLocalAndRemote: false,
+      unsyncedCurrentTab: "local",
+      keywordFilter: undefined,
+      categoryFilter: undefined,
     };
   },
   computed: {},
@@ -18,5 +39,36 @@ export default {
 </script>
 
 <template>
-  <div>hello dev!</div>
+  <div class="container">
+    <v-btn color="primary" @click="dialogOpen = true"> Save </v-btn>
+    <v-dialog
+      v-model="dialogOpen"
+      transition="dialog-bottom-transition"
+      max-width="600"
+    >
+      <v-card>
+        <v-card-text class="text-h6 grey lighten-2">
+          Save
+          <v-icon
+            @click="dialogOpen = false"
+            style="float: right; height: 30px"
+          >
+            mdi-close
+          </v-icon>
+        </v-card-text>
+        <v-divider></v-divider>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+}
+.v-dialog > .v-card > .v-card__text {
+  padding: 5px 20px;
+}
+</style>
